58dba0f4a06284f0ea0caa5ad11c1b517ea8a614
Johnny Appleseed <liuzhaohui@inspur.com>
2022-05-11 15:00:31 +0800

    Leave empty to use existent job name when install

--- install.bat
+++ install.bat
@@ -4,8 +4,18 @@ setlocal
 pushd "%~dp0"
 
 :input
-set /p "job=Enter Jenkins job name: "
-if "%job%" == "" goto input
+if defined JENKINS_JOB_NAME (
+    set /p "job=Enter Jenkins job name (leave empty to use %JENKINS_JOB_NAME%): "
+) else (
+    set /p "job=Enter Jenkins job name: "
+)
+if not defined job (
+    if defined JENKINS_JOB_NAME (
+        set "job=%JENKINS_JOB_NAME%"
+    ) else (
+        goto input
+    )
+)
 
 @echo Jenkins job name: %job%
 setx JENKINS_JOB_NAME "%job%" 1>nul
