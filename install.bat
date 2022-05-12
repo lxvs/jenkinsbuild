736ee95bbbe13d339e43931408a3b1a4b9121bcc
Johnny Appleseed <liuzhaohui@inspur.com>
2022-05-12 09:27:50 +0800

    Set JENKINS_WITH_PARAMETER in install.bat

--- install.bat
+++ install.bat
@@ -2,6 +2,7 @@
 setlocal
 
 set job=
+set withParam=
 
 pushd "%~dp0"
 
@@ -19,8 +20,32 @@ if not defined job (
     )
 )
 
+:is_with_param
+if defined JENKINS_WITH_PARAMETER if "%JENKINS_WITH_PARAMETER%" NEQ "0" (
+    set /p "withParam=Does your Jenkins job have parameters (Y/N, leave empty is Y): "
+) else (
+    set /p "withParam=Does your Jenkins job have parameters (Y/N, default is N): "
+)
+if not defined withParam (
+    if defined JENKINS_WITH_PARAMETER (
+        set "withParam=%JENKINS_WITH_PARAMETER%"
+    ) else (
+        goto is_with_param
+    )
+) else (
+    if /i "%withParam%" == "y" (
+        set "withParam=1"
+    ) else if /i "%withParam%" == "n" (
+        set "withParam=0"
+    ) else (
+        >&2 echo Please enter Y or N, or leave empty.
+        goto is_with_param
+    )
+)
+
 @echo Jenkins job name: %job%
 setx JENKINS_JOB_NAME "%job%" 1>nul
+setx JENKINS_WITH_PARAMETER "%withParam%" 1>nul
 
 if not exist "%USERPROFILE%\bin\" md "%USERPROFILE%\bin\"
 copy /Y "jenkinsbuild" "%USERPROFILE%\bin\" 1>nul
