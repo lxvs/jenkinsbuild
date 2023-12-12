5a126f402880b324a68e02b0185e22090f0ef9ad
Liu, Zhao-hui <lzhh@duck.com>
2023-12-12 18:05:11 +0800

    Update archive.sh as `jg archive' was removed

--- release/archive.sh
+++ release/archive.sh
@@ -8,29 +8,50 @@ archive_cleanup () {
 
 archive () {
     local fname
+    local exe7za="C:/Users/$USERNAME/AppData/Local/Programs/jai/7za.exe"
+    local description toplevel
     rm -rf tmp/ || return
     mkdir tmp || return
-    jg archive --with-submodules --stdout HEAD | tar -xC tmp/ || return
+    trap archive_cleanup INT TERM
+    toplevel=$(git rev-parse --show-toplevel) || return
+    description=$(git describe --always HEAD) || return
+    description=${description#v}
+    dirname=$(basename -- "$toplevel") || return
+    fname="$dirname-$description"
+    mkdir "tmp/$fname" || return
+    (
+        cd "$toplevel" || return
+        jg ls-files \
+            | grep -v '\(^\|/\).git\w\+$' \
+            | grep -v '^release/' \
+            | tr '\n' '\0' \
+            | xargs -0 cp --parents -r -t "$scriptdir/tmp/$fname" \
+            || return
+    ) || return
     (
         set +o noglob
-        trap archive_cleanup EXIT
-        cd "$(ls -d tmp/*)" || return
-        fname=$(basename -- "$PWD")
+        cd "tmp/$fname" || return
         (
             cd .. || return
             tar -zcf "../$fname.tgz" "$fname" || return
-            7za a -mx9 "../$fname.7z" "$fname" || return
+            "$exe7za" a -mx9 "../$fname.7z" "$fname" || return
         ) || return
-        7za a -mx9 "$name.7z" * || return
+        "$exe7za" a -mx9 "$name.7z" * || return
         cp "../../$name.sfx" . || return
         cmd //c "copy /b $name.sfx + $name.7z $(cygpath -w "../../$(basename $PWD).exe")" || return
     ) || return
 }
 
 print_adoc () {
+    local asciidoctor_pdf="C:/Ruby32-x64/bin/asciidoctor-pdf"
     ls ../*.adoc 1>/dev/null 2>&1 || return 0
-    asciidoctor-pdf -a scripts=cjk -a pdf-theme=cjk-theme.yml -a pdf-fontsdir=$LOCALAPPDATA\\Microsoft\\Windows\\fonts,$WINDIR\\fonts ../*.adoc -D .
+    "$asciidoctor_pdf" -a scripts=cjk \
+        -a pdf-theme=cjk-theme.yml \
+        -a pdf-fontsdir=$LOCALAPPDATA\\Microsoft\\Windows\\fonts,$WINDIR\\fonts \
+        ../*.adoc \
+        -D .
 }
+
 main () {
     local scriptdir name
     scriptdir=$(cd "$(dirname "$0")" && PWD) || exit
@@ -38,6 +59,7 @@ main () {
     cd "$scriptdir" || exit
     archive
     print_adoc
+    archive_cleanup
 }
 
 main "$@"
